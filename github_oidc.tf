1f238fdb8bae07782c7befc99b064fcf89ba8d9e
Robert Heng <robertheng3@gmail.com>
2026-09-06 17:33:49 -0400

    Delete github_oidc.tf

--- github_oidc.tf
+++
@@ -1,9 +0,0 @@
-
-//This might be order of creation issue: need the OIDC token to be created before being able to use CI/CD
-resource "aws_iam_openid_connect_provider" "github" {
-  url = "https://token.actions.githubusercontent.com"
-
-  client_id_list = [
-    "sts.amazonaws.com"
-  ]
-}
