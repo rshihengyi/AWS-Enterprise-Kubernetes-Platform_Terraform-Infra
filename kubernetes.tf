d80b761ca17c1470278d8433c85205b6be7e8734
Robert Heng <robertheng3@gmail.com>
2026-09-08 15:31:22 -0400

    Refactor EKS provider configuration using modules
    Updated EKS cluster configuration to use module outputs.

--- kubernetes.tf
+++ kubernetes.tf
@@ -1,24 +1,25 @@
-/*
-    Using data from state file for EKS Cluster
-*/
-data "terraform_remote_state" "eks" {
-  backend = "s3"
-  config = {
-    bucket = "enterprise-kubernetes-platform-tf-state"
-    key = "dev/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
+# /*
+#     Using data from state file for EKS Cluster
+# */
+# data "terraform_remote_state" "eks" {
+#   backend = "s3"
+#   config = {
+#     bucket = "enterprise-kubernetes-platform-tf-state"
+#     key = "dev/terraform.tfstate"
+#     region = "us-east-1"
+#   }
+# }
 
 
 
-data "aws_eks_cluster" "cluster" {
-  name = data.terraform_remote_state.eks.outputs.cluster_name
-}
+# data "aws_eks_cluster" "cluster" {
+#   name = data.terraform_remote_state.eks.outputs.cluster_name
+# }
+
 
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     command     = "aws"
@@ -26,7 +27,7 @@ provider "kubernetes" {
       "eks",
       "get-token",
       "--cluster-name",
-      data.aws_eks_cluster.cluster.name
+      module.eks.cluster_name
     ]
   }
 }
