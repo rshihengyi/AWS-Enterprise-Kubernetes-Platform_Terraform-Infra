de69b97c2f692971422728f55d8fa441b573d727
Robert Heng <robertheng3@gmail.com>
2026-09-08 15:41:25 -0400

    Refactor AWS and Helm provider configurations

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 // Source: https://developer.hashicorp.com/terraform/tutorials/kubernetes/helm-provider?in=terraform%2Fkubernetes
 provider "helm" {
   kubernetes = {
-//    config_path            = "~/.kube/config"
+  //  config_path            = "~/.kube/config"
     host                   = module.eks.cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
     /*
@@ -18,10 +18,19 @@ provider "helm" {
 
     exec = {
       api_version = "client.authentication.k8s.io/v1beta1"
-      args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name,
-      "--profile", "me"]
+      args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
       command = "aws"
     }
+  }
+}
+
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
   }
 }
 
