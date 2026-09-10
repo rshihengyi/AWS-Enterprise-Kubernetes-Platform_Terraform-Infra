157b9da8874c58d9ac575bc8a745d2861a6ae912
Robert Heng <robertheng3@gmail.com>
2026-09-10 14:20:17 -0400

    Add files via upload

--- s3_state_backend.tf
+++ s3_state_backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    profile      = "DevUser"
+    //profile      = "DevUser"
     bucket       = "enterprise-kubernetes-platform-tf-state"
     key          = "dev/terraform.tfstate"
     region       = "us-east-1"
