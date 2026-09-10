cbfd63188f0613692bf62fd34068ee4f9497ca38
Robert Heng <robertheng3@gmail.com>
2026-09-10 15:31:11 -0400

    use DevUser profile

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region = var.my_region
-  //profile = "DevUser"
+  profile = "DevUser"
 }
 
 // Source: https://developer.hashicorp.com/terraform/tutorials/kubernetes/helm-provider?in=terraform%2Fkubernetes
