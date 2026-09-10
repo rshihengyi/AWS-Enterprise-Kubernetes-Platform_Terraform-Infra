49c47359545d2871bfe2b66edaa4a59f21509e00
Robert Heng <robertheng3@gmail.com>
2026-09-10 16:50:55 -0400

    Add files via upload

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ provider "helm" {
 
     exec = {
       api_version = "client.authentication.k8s.io/v1beta1"
-      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name, "--profile", "DevUser"]
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
       command     = "aws"
     }
   }
